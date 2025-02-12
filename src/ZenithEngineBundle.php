899775804b0b5832c1142dec84a5b920fe1fb9c3
pg013c <golec.przemyslaw@gmail.com>
2025-02-12 22:05:51 +0100

    Reorganize engine bundle

---
+++ src/ZenithEngineBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Apsensa\Bundle\ZenithEngineBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class ZenithEngineBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // load an XML, PHP or YAML file
+        $container->import('../Resources/config/services.yml');
+        $container->import('../Resources/config/routes.yaml');
+
+//        // you can also add or replace parameters and services
+//        $container->parameters()
+//            ->set('acme_hello.phrase', $config['phrase'])
+//        ;
+//
+//        if ($config['scream']) {
+//            $container->services()
+//                ->get('acme_hello.printer')
+//                ->class(ScreamingPrinter::class)
+//            ;
+//        }
+    }
+}
