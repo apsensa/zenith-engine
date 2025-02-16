c29e12e906eefeb65e3d78c9a8eb891fcaf9ccb7
pg013c <golec.przemyslaw@gmail.com>
2025-02-16 10:44:39 +0100

    Clean a little bit composer.json

--- src/ZenithEngineBundle.php
+++ src/ZenithEngineBundle.php
@@ -12,21 +12,6 @@ class ZenithEngineBundle extends AbstractBundle
 {
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        // load an XML, PHP or YAML file
-//        $container->import('Resources/config/config.yaml');
-//        $container->import('Resources/config/services.yaml');
-//        $container->import('Resources/config/routes.yaml');
-
-//        // you can also add or replace parameters and services
-//        $container->parameters()
-//            ->set('acme_hello.phrase', $config['phrase'])
-//        ;
-//
-//        if ($config['scream']) {
-//            $container->services()
-//                ->get('acme_hello.printer')
-//                ->class(ScreamingPrinter::class)
-//            ;
-//        }
+        $container->import('Resources/config/services.yaml');
     }
 }
